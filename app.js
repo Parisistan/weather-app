e6cc3463fa2b45f46ba8804c85f98566a3cb49b6
Parisistan <71983551+Parisistan@users.noreply.github.com>
2022-03-02 11:43:39 +0000

    undefined forecast days

--- app.js
+++ app.js
@@ -24,23 +24,37 @@ if (hours < 10) {
 let nowTime = document.querySelector("#now");
 nowTime.innerHTML = `${weekDays}, ${hours}:${minutes}`;
 
-function displayWeekly() {
+function formatted(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+
+}
+
+function displayWeekly(response) {
+
+    let forcastDays = response.data.daily;
+
     let weeklyElement = document.querySelector("#weekly");
 
     let weeklyLoop = `<div class = "row">`;
 
-    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    days.forEach(function(day) {
+
+    forcastDays.forEach(function(forcastDay) {
         weeklyLoop = weeklyLoop + `
     
         <div class="col-2">
             <div class="week-day">
-            ${day}
+            ${formatted(forcastDay.dt)}
             </div>
-                <img src="" alt="" width="45px">
+                <img 
+                src = "http://openweathermap.org/img/wn/${forcastDay.weather[0].icon}@2x.png"
+                alt=""
+                width="45px">
             <div class="week-day-temp">
-            <span class="highest">18º </span>
-            <span class="lowest">12º </span>
+            <span class="highest"> ${forcastDay.temp.max}º </span>
+            <span class="lowest"> ${forcastDay.temp.min}º </span>
             </div>
         </div>`
 
@@ -49,6 +63,17 @@ function displayWeekly() {
     weeklyLoop = weeklyLoop + `</div>`;
     weeklyElement.innerHTML = weeklyLoop;
 }
+
+function getWeekly(coordinates) {
+
+    console.log(coordinates);
+
+    let apiKey = "5f80099813d146f19a4a94f45520a87e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayWeekly);
+
+}
+
 
 function showWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
@@ -61,6 +86,8 @@ function showWeather(response) {
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
+    getWeekly(response.data.coord);
+
 }
 
 function searchIt(event) {
@@ -71,7 +98,6 @@ function searchIt(event) {
     axios.get(apiUrl).then(showWeather);
 }
 
-displayWeekly();
 
 let searchingCity = document.querySelector("#search-engine");
 searchingCity.addEventListener("submit", searchIt);
